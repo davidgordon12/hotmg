6e0ed40ade7bd48fe5d0744fd6fde965c0cce12f
David Gordon <davidgordondev@gmail.com>
2025-03-06 15:01:13 -0500

    Add DPS Graph src

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GraphComponent } from './graph/graph.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GraphComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

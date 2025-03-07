d879643c44eb55adb81510f30cc96f22534f6275
David Gordon <davidgordondev@gmail.com>
2025-03-06 19:20:17 -0500

    Kinda sorta make the graph component

--- src/app/graph/graph.component.ts
+++ src/app/graph/graph.component.ts
@@ -3,9 +3,25 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-graph',
-  template: `<h1>GRAPH</h1>`,
+  template: `
+  <div class="ml-24 bg-zinc-900 w-screen h-28">
+    <i class="mt-6 text-3xl tracking-widest text-zinc-100 font-bold p-6">Att: </i>
+    <input type="number" class="mt-6 p-1 text-zinc-300 text-2xl w-16 mr-40 border-b-4 border-zinc-100">
+    <i class="mt-6 text-3xl tracking-widest text-zinc-100 font-bold p-6">Dex: </i>
+    <input type="number" class="mt-6 p-1 text-zinc-300 text-2xl w-16 mr-40 border-b-4 border-zinc-100">
+    <i class="mt-6 text-3xl tracking-widest text-zinc-100 font-bold p-6">DMG: </i>
+    <input pattern="^[1-9]\d{0,2}(?:-[1-9]\d{0,2})?$" class="mt-6 p-1 text-zinc-300 text-2xl w-16 mr-40 border-b-4 border-zinc-100">
+    <button class="mt-6 p-1 font-bold italic text-zinc-300 text-3xl">Go</button>
+  </div>
+  <div class="h-screen w-screen bg-zinc-800">
+  </div>
+`,
   styles: ``
 })
 export class GraphComponent {
+  dmg: number = 0;
+  dex: number = 0;
+  att: number = 0;
 
+  dps: number = (this.dmg + this.att) * (this.dex / 100)
 }
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -5,9 +5,8 @@ import { RouterLink } from '@angular/router';
   selector: 'app-sidebar',
   imports: [RouterLink],
   template: `
-    <div class="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-green-200 text-pink-950 shadow-lg text-3xl">
-        <a routerLink="/">Graph</a>
-        <a routerLink="/fake">NotExisting</a>
+    <div class="fixed top-0 left-0 h-screen w-24 m-0 flex flex-col bg-zinc-950 text-pink-950 shadow-lg text-3xl">
+        <a routerLink="/"><img src="https://realmshop.info/wp-content/uploads/2019/01/56.png" /></a>
     </div>
   `,
   styles: ``
